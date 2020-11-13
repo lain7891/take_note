aa0e4118c87454f039e24ba63f16849a0ce4b72e
Estefania <nialupo87@gmail.com>
2020-11-13 10:04:42 -0500

    listened on the port

--- server.js
+++ server.js
@@ -2,4 +2,16 @@ const express = require("express");
 
 const PORT = process.env.PORT || 3000;
 
+// create instance of express
 const app = express();
+
+
+
+
+
+
+
+// listen on the port
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
